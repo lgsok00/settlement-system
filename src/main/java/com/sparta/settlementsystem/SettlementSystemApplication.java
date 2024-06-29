307c1a9ba6b66685c0dba069c2bd2f9879c6e1d5
lgsok00 <lgsok00@naver.com>
2024-06-29 12:35:01 +0900

    init project

---
+++ src/main/java/com/sparta/settlementsystem/SettlementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.settlementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SettlementSystemApplication.class, args);
+  }
+
+}
